895e744cf43c4b54cef8fa6e3b39ed35d6995203
Matias Kudaka <111797730+maticba@users.noreply.github.com>
2023-05-01 20:12:47 -0300

    Seguimos trabajando con Python
    Modificacion archivo Leccion02

--- LABORATORIO/PYTHON/Leccion02/archivos_con_with.py
+++ LABORATORIO/PYTHON/Leccion02/archivos_con_with.py
@@ -5,7 +5,7 @@ from ManejoArchivos import ManejoArchivos
 #No hace falta ni el try, ni el finally
 #En el contexto de with lo que se ejecuta de manera automatica
 # Utiliza diferentes metodos: __enter__ es el que abre
-#otro metod es __exit__ que es el que cierra
+#otro metod es __exit__ que es el que cierra.
 
 with ManejoArchivos("prueba.txt") as archivo:
     print(archivo.read())
