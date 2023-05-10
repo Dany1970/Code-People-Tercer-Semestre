57791a24722946dea540a5216978ecf0131a313c
Beatriz <111789906+CreatemyGithub19@users.noreply.github.com>
2023-05-10 18:25:29 -0300

    commiteamos ejercicios de Java
    Añado al repositorio la clase 3 de Java

---
+++ PROGRAMACION/Lección03/ModifiacdoresAcceso/src/paquete1/Clase1.java
@@ -0,0 +1,17 @@
+
+package paquete1;
+
+public class Clase1 {
+    public String atributoPublic = "Valor atributo public";
+    
+  public Clase1(){
+      System.out.println("Constructor public");
+  }  
+        
+  public  void metodoPublico(){ 
+      System.out.println("Método public");
+        
+  }
+}
+    
+
