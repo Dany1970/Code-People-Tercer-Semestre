e7cf506e41dbcdbd66f5ceb0675075e1d509b595
Matias Kudaka <111797730+maticba@users.noreply.github.com>
2023-06-28 09:19:46 -0300

    Revert "Tarea Laboratorio III. Clase 9 puntos 4,5,6"
    This reverts commit 41db05f55a0dcfc2b65a1100d7294769602a25ad.

--- LABORATORIO/PYTHON/Lección07/capa_datos_persona/persona_dao.py
+++ LABORATORIO/PYTHON/Lección07/capa_datos_persona/persona_dao.py
@@ -1,6 +1,4 @@
-from capa_datos_persona.Persona import Persona
-from capa_datos_persona.conexion import Conexion
-from logger_base import log
+import logging
 
 
 class PersonaDAO:
@@ -18,8 +16,8 @@ class PersonaDAO:
     _ELIMINAR = 'DELETE FROM persona WHERE id_persona=%s'
 
     # Definimos los métodos de la clase
-    @classmethod
-    def seleccionar(cls):
+@classmethod
+def seleccionar(cls):
         with Conexion.obtenerConexion():
             with Conexion.obtenerCursor() as cursor:
                 cursor.execute(cls._SELECCIONAR)
@@ -30,8 +28,8 @@ class PersonaDAO:
                     personas.append(persona)
                     return personas
 
-    @classmethod
-    def insertar(cls, persona):
+@classmethod
+def insertar(cls, persona):
         with Conexion.obtenerConexion():
             with Conexion.obtenerCursor() as cursor:
                 valores = (persona.nombre, persona.apellido, persona.email)
@@ -39,44 +37,11 @@ class PersonaDAO:
                 log.debug(f'Persona Insertada: {persona}')
                 return cursor.rowcount
 
-    @classmethod
-    def actualizar(cls, persona):
-        with Conexion.obtenerConexion():
-            with Conexion.obtenerCursor() as cursor:
-                valores = (persona.nombre, persona.apellido, persona.email, persona.id_persona)
-                cursor.execute(cls._ACTUALIZAR, valores)
-                log.debug(f'Persona actualizada: {persona}')
-                return cursor.rowcount
-
-    @classmethod
-    def eliminar(cls,persona):
-        with Conexion.obtenerConexion():
-            with Conexion.obtenerCursor() as cursor:
-                valores = (persona.id_persona,)
-                cursor.execute(cls._ELIMINAR, valores)
-                log.debug(f'Los objetos eliminados son: {persona}')
-                return cursor.rowcount
-
-if __name__ == '__main__':
-    #Eliminar un registro
-    persona1 = Persona(id_persona=8)
-    personas_eliminadas = PersonaDAO.eliminar(persona1)
-    log.debug(f'Personas Eliminadas: {personas_eliminadas}')
-
-
-
-if __name__ == '__main__':
-    #Actualizar un registro
-    persona1 = Persona(1, 'Juan Jose', 'Pena', 'jjpena@mail.com')
-    personas_actualizadas = PersonaDAO.actualizar(persona1)
-    log.debug(f'Personas actualizadas: {personas_actualizadas}')
-
-
 if __name__ == '__main__':
     # Insertar un registro
-    #persona1 = Persona(nombre='Pedro', apellido='Romero', email='promero@gmail.com')
-    #personas_insertadas = PersonaDAO.insertar(persona1)
-    #log.debug(f'Personas insertadas: {personas_insertadas}')
+    persona1 = Persona(nombre='Pedro', apellido='Romero', email='promero@gmail.com')
+    personas_insertadas = PersonaDAO.insertar(persona1)
+    log.debug(f'Personas insertadas: {personas_insertadas}')
 
     # Seleccionar objetos
     personas = PersonaDAO.seleccionar()
